ebfa71e6b7aa9ceef028d20e420c5b4ef5a01108
Sander van der Maar <sandervandermaar@gmail.com>
2021-09-04 09:43:22 -0300

    Now generates the squares with code

--- scatter.py
+++ scatter.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4, s=200)
+x_values = list(range(1, 1001))
+y_values = []
+for x in x_values:
+    y_values.append(x * x)
+
+plt.scatter(x_values, y_values, s=100)
 
 plt.title("Square Numbers", fontsize=24)
 
