29a54972b3b33d306e13bce0354074ac732b03d5
Sander van der Maar <sandervandermaar@gmail.com>
2021-09-11 09:35:59 -0300

    Added a second die

--- pygal1.py
+++ pygal1.py
@@ -9,23 +9,26 @@ class Die():
         return randint(1, self.num_sides)
 
 die = Die()
+die2 = Die()
 
 results = []
-for roll_num in range(1000):
-    result = die.roll()
+for roll_num in range(50 * 1000):
+    result = die.roll() + die2.roll()
     results.append(result)
 
+max_result = die.num_sides + die2.num_sides
+
 frequencies = []
-for value in range(1, die.num_sides+1):
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 hist = pygal.Bar()
 
-hist.title = "Result of rolling one D6 1000 times."
-hist.x_labels = ["1", "2", "3", "4", "5", "6"]
+hist.title = "Result of rolling two D6 1000 times."
+hist.x_labels = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
-hist.add("D6", frequencies)
-hist.render_to_file("die_visual.svg")
+hist.add("D6 + D6", frequencies)
+hist.render_to_file("die_visual2.svg")
