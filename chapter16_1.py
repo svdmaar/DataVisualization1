4c2841a2c3fde3011faafe8cdbfb4551fef4449c
Sander van der Maar <sandervandermaar@gmail.com>
2021-09-18 09:32:00 -0300

    Adds some nice blue color between the datasets

--- chapter16_1.py
+++ chapter16_1.py
@@ -2,12 +2,14 @@ import csv
 from matplotlib import pyplot as plt
 from datetime import datetime
 
-filename = "sitka_weather_07-2014.csv"
+#filename = "sitka_weather_07-2014.csv"
+filename = "sitka_weather_2014.txt"
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
     highs = []
+    lows = []
     dates = []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
@@ -16,10 +18,15 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
 
-fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c="red")
+        low = int(row[3])
+        lows.append(low)
 
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c="red", alpha=0.5)
+plt.plot(dates, lows, c="blue", alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
+
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
 plt.xlabel("", fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
