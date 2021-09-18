1142794927472849fd312f9c63fd33ca457c8413
Sander van der Maar <sandervandermaar@gmail.com>
2021-09-18 09:25:05 -0300

    Parsed datetimes

--- chapter16_1.py
+++ chapter16_1.py
@@ -1,5 +1,6 @@
 import csv
 from matplotlib import pyplot as plt
+from datetime import datetime
 
 filename = "sitka_weather_07-2014.csv"
 with open(filename) as f:
@@ -7,15 +8,20 @@ with open(filename) as f:
     header_row = next(reader)
 
     highs = []
+    dates = []
     for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+
         high = int(row[1])
         highs.append(high)
 
-plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c="red")
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c="red")
 
 plt.title("Daily high temperatures, July 2014", fontsize=24)
 plt.xlabel("", fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis="both", which="major", labelsize=16)
 
