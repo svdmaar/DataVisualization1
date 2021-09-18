b0000ad6b95c27323a28ca2dfc57df256d197db9
Sander van der Maar <sandervandermaar@gmail.com>
2021-09-18 09:10:43 -0300

    Improved our graph

--- chapter16_1.py
+++ chapter16_1.py
@@ -11,7 +11,12 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
 
-plt.figure(dpi=128, figsize=(20, 20))
-plt.plot(highs)    
+plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(highs, c="red")
+
+plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.xlabel("", fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis="both", which="major", labelsize=16)
 
 plt.show()
