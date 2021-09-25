0be45254499bdd871cee94183d4521aae7871a94
Sander van der Maar <sandervandermaar@gmail.com>
2021-09-25 11:14:29 -0300

    Adds first requests demo

--- popdata.py
+++ popdata.py
@@ -1,6 +1,7 @@
-import json
+import json, pygal
 from country_code import get_country_code
-import pygal
+#import country_code
+from pygal.style import RotateStyle, LightColorizedStyle
 
 filename = "population_data.json"
 with open(filename) as f:
@@ -16,8 +17,26 @@ for pop_dict in pop_data:
         if code:
             cc_populations[code] = population
 
-wm = pygal.maps.world.World()
+cc_pops_1 = {}
+cc_pops_2 = {}
+cc_pops_3 = {}
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
+wm_style = RotateStyle("#336699", base_style=LightColorizedStyle)
+#wm_style = RotateStyle("#336699")
+#wm_style = LightColorizedStyle
+wm = pygal.maps.world.World(style = wm_style)
 wm.title = "World Population in 2010, by country"
-wm.add("2010", cc_populations)
+wm.add("0-10m", cc_pops_1)
+wm.add("10m-1bn", cc_pops_2)
+wm.add(">1bn", cc_pops_3)
 
 wm.render_to_file("world_population.svg")
---
+++ requests1.py
@@ -0,0 +1,12 @@
+import requests
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print("Status code:", r.status_code)
+
+response_dict = r.json()
+
+print(response_dict.keys())
+
+for i in response_dict["items"]:
+    print(i)
