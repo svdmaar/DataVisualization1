f7dde5ebaca334e625f82552866aadb671cbf52f
Sander van der Maar <sandervandermaar@gmail.com>
2021-10-02 09:29:40 -0300

    Lists a lot of information about the repos

--- requests1.py
+++ requests1.py
@@ -1,6 +1,8 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
-url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
 r = requests.get(url)
 print("Status code:", r.status_code)
 
@@ -8,5 +10,23 @@ response_dict = r.json()
 
 print(response_dict.keys())
 
-for i in response_dict["items"]:
-    print(i)
+print("Total repos: " + str(response_dict["total_count"]))
+
+repo_dicts = response_dict["items"]
+print("Repositories returned: " + str(len(repo_dicts)))
+
+repo_dict = repo_dicts[0]
+print("Keys: " + str(len(repo_dict)))
+for key in sorted(repo_dict.keys()):
+    print(key)
+
+names = []
+stars = []
+for repo_dict in repo_dicts:
+    print("Name: " + repo_dict["name"])
+    print("Owner: " + repo_dict["owner"]["login"])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Created:', repo_dict['created_at'])
+    print('Updated:', repo_dict['updated_at'])
+    print('Description:', repo_dict['description'])
