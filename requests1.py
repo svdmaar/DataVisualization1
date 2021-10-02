cbe65be9eca1f29a33047c29311f6a2296b18a8b
Sander van der Maar <sandervandermaar@gmail.com>
2021-10-02 09:47:35 -0300

    Creates a nice graph with clickable links

--- requests1.py
+++ requests1.py
@@ -21,12 +21,32 @@ for key in sorted(repo_dict.keys()):
     print(key)
 
 names = []
-stars = []
+#stars = []
+plot_dicts = []
 for repo_dict in repo_dicts:
-    print("Name: " + repo_dict["name"])
-    print("Owner: " + repo_dict["owner"]["login"])
-    print('Stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
-    print('Created:', repo_dict['created_at'])
-    print('Updated:', repo_dict['updated_at'])
-    print('Description:', repo_dict['description'])
+    names.append(repo_dict["name"])
+    #stars.append(repo_dict["stargazers_count"])
+    plot_dicts.append({
+        "value": repo_dict["stargazers_count"],
+        "label": repo_dict["description"],
+        "xlink": repo_dict["html_url"]
+        })
+
+my_style = LS("#333366", base_style=LCS)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = "Most-starred Python Projects on GitHub"
+chart.x_labels = names
+
+#chart.add("", stars)
+chart.add("", plot_dicts)
+chart.render_to_file("python_repos.svg")
